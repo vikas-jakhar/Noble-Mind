34934a69cea45f9b798220ebd0720ef5cdbeecee
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-05-11 14:43:21 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      background: {
+        'line-gradient': "linear-gradient(130.88deg, #A854E9 - 9.14 %, #4F91FC 283.16 %);",
+      }
+    },
+  },
+  plugins: [],
+}
+
+
